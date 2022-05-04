235245671e8c0353b8ec893e54bff3cf8653a28d
Aleksandr Orlovets <orlovetsjob@gmail.com>
2022-05-04 23:27:13 +0300

    add dist

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: false, // использовать точку с запятой ('true')
-  singleQuote: true, // использовать одинарные кавычки ('false')
-  arrowParens: 'avoid', // x => x (always - example: (x) => x)
-  // arrowParens: 'avoid', // заключать ли в скобки стрелочную функцию (x) => x ('always|avoid')
-  // trailingComma: 'all', // висящие запятые ('es5', 'es5|none|all')
-  // bracketSameLine: true, // перенос > на другую строку при длинной строке ('false')
-  // printWidth: 120, // длина переноса строки (80)
-}
---
+++ assets/AboutView.c555e29c.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as s,p as _,a,b as c}from"./index.140b5b00.js";const d={},n=e=>(_("data-v-71f04ee4"),e=e(),a(),e),p={class:"about"},r=n(()=>c("h1",null,"This is an about page",-1)),i=[r];function u(e,l){return t(),s("div",p,i)}var h=o(d,[["render",u],["__scopeId","data-v-71f04ee4"]]);export{h as default};
